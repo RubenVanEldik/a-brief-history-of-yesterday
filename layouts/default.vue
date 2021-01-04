b0bcfbb325b9f5f2ebaedec8cb53d99c73106b42
Ruben <contact@abriefhistoryofyesterday.com>
2021-01-04 21:16:54 +0100

    feat: Add a basic default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,14 @@
 <template>
-  <nuxt />
+  <div class="min-h-screen w-screen m-auto flex flex-col sm:w-11/12 md:w-4/5 lg:w-3/4 xl:w-2/3">
+    <main class="flex-grow">
+      <nuxt />
+    </main>
+    <footer class="mb-4 text-sm text-center text-gray-500 italic">
+      <nuxt-link to="/contact">
+        Made with ♥ in Utrecht
+      </nuxt-link>
+    </footer>
+  </div>
 </template>
 
 <script>
