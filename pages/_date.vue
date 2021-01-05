4158cd436a9952d0920cbae8fead8f87a5beb000
Ruben <contact@abriefhistoryofyesterday.com>
2021-01-05 16:17:52 +0100

    feat: Add a page for previous dates

---
+++ pages/_date.vue
@@ -0,0 +1,22 @@
+<template>
+  <day
+    v-if="$route.params.date"
+    :date="$route.params.date"
+  />
+</template>
+
+<script>
+import dayjs from 'dayjs'
+
+export default {
+  validate ({ params }) {
+    return /^(19|20)\d\d-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/.test(params.date)
+  },
+  middleware: ({ route, redirect }) => {
+    const yesterday = dayjs().subtract(1, 'day').format('YYYY-MM-DD')
+    if (route.params.date === yesterday) {
+      redirect('/')
+    }
+  }
+}
+</script>
