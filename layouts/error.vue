9888c0a0cf186bac7e569203722cfa88c652ce70
Ruben <contact@abriefhistoryofyesterday.com>
2021-01-05 01:53:01 +0100

    feat: Add an error page

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center pt-48 sm:pt-56 md:pt-64 text-gray-600">
+    <div
+      class="text-4xl mb-2"
+      v-text="error.statusCode"
+    />
+    <div
+      class="text-lg"
+      v-text="error.message"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
