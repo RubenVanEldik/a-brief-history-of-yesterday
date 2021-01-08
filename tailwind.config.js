694a0b10737ed6b54f2fd9654e1de27e72f405c4
Ruben <contact@abriefhistoryofyesterday.com>
2021-01-08 11:20:21 +0100

    fix: Use trueGray as the gray color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   theme: {
     fontFamily: {
       sans: ['Charter', 'Garamond', 'serif']
+    },
+    extend: {
+      colors: {
+        gray: colors.trueGray
+      }
     }
   },
   plugins: [
